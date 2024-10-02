9b17b5e1cf2f553bd4a65634299b30a114dbb05b
huyngo14122002 <huyngo14122002@gmail.com>
2024-10-02 18:07:09 +0700

    UpdateByHuyNgo2.10.2024

--- Models/Material/Concrete.cs
+++ Models/Material/Concrete.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using VibrantBIM.ViewModels;
 
 namespace VibrantBIM.Models.Material
 {
     [XmlInclude(typeof(Concrete))]
-    public class Concrete
+    public class Concrete : ViewModelBase
     {
-
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
     }
 }
